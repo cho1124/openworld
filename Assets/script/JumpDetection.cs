ad9b191f6f1f438f1549e9eff2be034a986c4d1b
cho1124 <122534567+cho1124@users.noreply.github.com>
2024-02-08 02:30:15 +0900

    ontrigger
    빈 오브젝트에 collider를 달고 istrigger를 켜놓고
    그 다음에 ontriggerEnter, ontriggerExit 등을 이용해 태그를 감지하도록 하였음

--- Assets/script/JumpDetection.cs
+++ Assets/script/JumpDetection.cs
@@ -7,22 +7,37 @@ public class JumpDetection : MonoBehaviour
     public bool isGrounded; // 바닥에 닿아 있는지 여부를 저장하는 변수
 
     // Collider가 다른 Collider에 닿았을 때 호출되는 메서드
-    private void OnCollisionEnter(Collision collision)
+    //private void OnCollisionEnter(Collision collision)
+    //{
+    //    // 바닥에 닿아 있는지 확인
+    //    if (collision.gameObject.CompareTag("Ground"))
+    //    {
+    //        isGrounded = true;
+    //    }
+    //}
+
+    private void OnTriggerEnter(Collider other)
     {
-        // 바닥에 닿아 있는지 확인
-        if (collision.gameObject.CompareTag("Ground"))
+        if (other.gameObject.CompareTag("Ground"))
         {
             isGrounded = true;
         }
     }
-
-    // Collider가 다른 Collider에 떼어졌을 때 호출되는 메서드
-    private void OnCollisionExit(Collision collision)
+    private void OnTriggerExit(Collider other)
     {
-        // 바닥에서 떼어졌는지 확인
-        if (collision.gameObject.CompareTag("Ground"))
+        if (other.gameObject.CompareTag("Ground"))
         {
             isGrounded = false;
         }
     }
+
+    // Collider가 다른 Collider에 떼어졌을 때 호출되는 메서드
+    //private void OnCollisionExit(Collision collision)
+    //{
+    //    // 바닥에서 떼어졌는지 확인
+    //    if (collision.gameObject.CompareTag("Ground"))
+    //    {
+    //        isGrounded = false;
+    //    }
+    //}
 }
