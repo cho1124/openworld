a444bea38427db5376fda8d2f647c42d11d0567c
cho1124 <122534567+cho1124@users.noreply.github.com>
2024-02-14 02:00:16 +0900

    애니메이션 적용
    간단한 애니메이션
    기본적으로 서 있는 동작과 방향키를 누르면 움직이는 동작, 점프키를 누르면 점프하는 동작, 왼쪽 시프트를 누르면 달리는 동작을 추가하였음

--- Assets/script/CharacterMove.cs
+++ Assets/script/CharacterMove.cs
@@ -9,19 +9,49 @@ public class CharacterMove : MonoBehaviour
 
     private Rigidbody rb;
     private bool isJumped = false;
+    private JumpDetection a;
+
+    Animator m_Animator;
+
 
     private void Start()
     {
         rb = GetComponent<Rigidbody>();
+
+        a = GetComponentInChildren<JumpDetection>();
+
+        m_Animator = GetComponent<Animator>();
     }
 
     void Update()
     {
         float currentSpeed = Input.GetKey(KeyCode.LeftShift) ? speed * 2.0f : speed;
 
+        
+        if(Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            
+            m_Animator.SetBool("Run", true);
+        }
+        if(Input.GetKeyUp(KeyCode.LeftShift))
+        {
+            m_Animator.SetBool("Run", false);
+        }
+        
+        
+
+
+
         // WSAD 키 입력을 감지하여 이동 방향을 설정
         float horizontal = Input.GetAxis("Horizontal");
         float vertical = Input.GetAxis("Vertical");
+
+
+        if (vertical != 0)
+        {
+            m_Animator.SetFloat("Speed", Mathf.Max(Mathf.Abs(horizontal), Mathf.Abs(vertical)));
+            //애니메이션 재생
+        }
 
         // 이동 방향에 속도를 곱하여 실제 이동 속도를 계산
         
@@ -31,10 +61,14 @@ public class CharacterMove : MonoBehaviour
         // 현재 위치에 이동량을 더해 캐릭터를 이동시킴
         transform.Translate(movement);
 
-        if (Input.GetKeyDown(KeyCode.Space)) // 스페이스바를 누르면
+        if (a.isGrounded && Input.GetKeyDown(KeyCode.Space))
         {
+            m_Animator.SetTrigger("Jump");
             Jump(); // 점프 함수 호출
         }
+
+        
+
 
 
     }
