5e554abe18838230de914b7bacb4a2f5354bca83
cho1124 <122534567+cho1124@users.noreply.github.com>
2024-03-14 01:25:09 +0900

    활 장착하고 칼 장착하는 모션 추가
    제목과 동일
    추가로 활을 장착할 동안에 그에 맞는 행동 모션들을 취할 수 있도록 하려고 함

--- Assets/script/Character/WeaponEquip.cs
+++ Assets/script/Character/WeaponEquip.cs
@@ -6,9 +6,12 @@ public class WeaponEquip : MonoBehaviour
 {
     public Transform backPosition;
     public Transform leftHandPosition;
+    public Transform rightHandPosition;
     public GameObject bow;
+    public GameObject sword;
 
     private bool equipBow = false;
+    private bool equipSword = false;
     private Animator animator; // 애니메이터 컴포넌트에 대한 참조
 
     void Start()
@@ -16,38 +19,41 @@ public class WeaponEquip : MonoBehaviour
         animator = GetComponent<Animator>(); // 애니메이터 컴포넌트 가져오기
     }
 
-    void Update()
+    private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Z))
+        if (Input.GetKeyDown(KeyCode.Z) && !equipBow && bow != null)
         {
-            equipBow = true; // 활 장착을 요청
-
-            if(bow != null)
-            {
-                
-                animator.SetTrigger("EquipBow");
-
-                Invoke("MoveBowToLeftHand", 0.5f); // 1초 후에 MoveBowToLeftHand 메서드 호출
-            }
-
+            ReleaseSword();
+            equipBow = true;
         }
 
-        if (Input.GetKeyDown(KeyCode.X))
+        if (Input.GetKeyDown(KeyCode.X) && !equipSword && sword != null)
         {
-            if (bow != null)
-            {
-                animator.SetTrigger("ReleaseBow");
 
-                Invoke("MoveBowToBack", 0.5f); // 1초 후에 MoveBowToLeftHand 메서드 호출
+            ReleaseBow();
+            equipSword = true;
 
-
-
-                equipBow = false; // 활 해제
-            }
         }
-
-        
     }
+
+
+    private void ReleaseBow()
+    {
+        equipBow = false;
+        animator.SetTrigger("ReleaseBow");
+        animator.SetBool("UseSword", true);
+    }
+
+    
+
+    private void ReleaseSword()
+    {
+        equipSword = false;
+        animator.SetTrigger("ReleaseSword");
+        animator.SetBool("UseSword", false);
+    }
+
+
 
     void MoveBowToLeftHand()
     {
@@ -62,5 +68,20 @@ public class WeaponEquip : MonoBehaviour
         bow.transform.localPosition = new Vector3(-0.112f, 0.307f, -0.132f); // 이 값을 (-0.2, 0, -0.15)
         bow.transform.localRotation = Quaternion.Euler(-62.128f, 64.645f, -40.307f);
     }
+
+    void MoveSwordToRightHand()
+    {
+        sword.transform.SetParent(rightHandPosition);
+        sword.transform.localPosition = Vector3.zero;
+        sword.transform.localRotation = Quaternion.identity;
+    }
+
+    void MoveSwordToBack() 
+    {
+        sword.transform.SetParent(backPosition);
+        sword.transform.localPosition = new Vector3(-0.1402862f, 0.145049f, 0.1862096f); // 이 값을 (-0.2, 0, -0.15)
+        sword.transform.localRotation = Quaternion.Euler(-3.94f, -178.37f, -67.721f);
+    }
+
 
 }
